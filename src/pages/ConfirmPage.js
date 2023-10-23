4bfe77508fd6d87439b30944ffee199ebc7f6822
nyddanao11 <112321001+nyddanao11@users.noreply.github.com>
2023-10-23 08:32:51 +0100

    Update ConfirmPage.js

--- src/pages/ConfirmPage.js
+++ src/pages/ConfirmPage.js
@@ -1,34 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import {Link} from'react-router-dom';
+import axios from 'axios';
 
 const ConfirmPage = () => {
   const [confirmationStatus, setConfirmationStatus] = useState('pending');
 
-  useEffect(() => {
-    // Here, you can make a request to your server to confirm the email.
-    // You can use the 'token' parameter from the URL to identify the user.
+ useEffect(() => {
+  const token = new URLSearchParams(window.location.search).get('token');
+  console.log(token);
 
-    // Request using the 'fetch' API:
-    const token = new URLSearchParams(window.location.search).get('token');
-     console.log(token);
-     
-    if (token) {
-    
-  fetch(`https://yeilva-store-server.up.railway.app/api/confirm?token=${token}`)
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.message === 'Email verified successfully') {
-            setConfirmationStatus('success');
-          } else {
-            setConfirmationStatus('error');
-          }
-        })
-        .catch((error) => {
-          console.error('Error confirming email:', error);
+  if (token) {
+    // Define the server address (replace with your server's actual address)
+    const serverAddress = 'https://yeilva-store-server.up.railway.app';
+
+    // Make the GET request using Axios
+    axios.get(`${serverAddress}/api/confirm?token=${token}`)
+      .then((response) => {
+        const data = response.data;
+        if (data.message === 'Email verified successfully') {
+          setConfirmationStatus('success');
+        } else {
           setConfirmationStatus('error');
-        });
-    }
-  }, []);
+        }
+      })
+      .catch((error) => {
+        console.error('Error confirming email:', error);
+        setConfirmationStatus('error');
+      });
+  }
+}, []);
+
 
   return (
     <div>
