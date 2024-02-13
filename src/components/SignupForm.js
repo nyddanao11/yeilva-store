fa6ffede072b754f73069fef3e8d006cb2bc78d3
nyddanao11 <112321001+nyddanao11@users.noreply.github.com>
2024-02-13 23:51:55 +0000

    13feb2024

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,60 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Button, Container, Row, Col, Spinner } from 'react-bootstrap';
+import {Form, Button, Container, Row, Col, Spinner } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Footer from'./Footer';
+import { Formik, Field, ErrorMessage} from 'formik';
+import * as Yup from 'yup';
+
 
 const SignUpForm = () => {
   const [serverResponse, setServerResponse] = useState('');
   const [isLoginSuccessful, setIsLoginSuccessful] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  // const backgroundImage = `${process.env.PUBLIC_URL}/background1.png`;
-
-  // Create state variables for form fields
-  const [formData, setFormData] = useState({
-    firstname: '',
-    lastname: '',
-    email: '',
-    password: '',
+  const validationSchema = Yup.object().shape({
+    firstname: Yup.string().required('First Name is required'),
+    lastname: Yup.string().required('Last Name is required'),
+    email: Yup.string().email('Invalid email').required('Email is required'),
+    password: Yup.string().required('Password is required').min(8, 'Password must be at least 8 characters'),
   });
 
-  // Handle form field changes
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
 
-
- const handleSubmit = (event) => {
-  event.preventDefault();
-
-  setLoading(true); // Set loading to true when the form is submitted
-
-  axios.post('https://yeilva-store-server.up.railway.app/register', formData)
-    .then((response) => {
-      if (response.status === 200) {
-        setIsLoginSuccessful(true);
-      } else {
-        setIsLoginSuccessful(false);
-        setServerResponse(response.data.error); // Set the error message
-      }
-    })
-    .catch((error) => {
-      console.error('Error submitting form data:', error);
-      setIsLoginSuccessful(false);
-      setServerResponse(' Error: Email already registered ');
-    })
-    .finally(() => {
-      setLoading(false); // Set loading to false when the request is complete (success or failure)
-    });
-};
-
-
-
+  
   useEffect(() => {
     if (isLoginSuccessful) {
       alert('Please confirm your email to login');
@@ -63,6 +29,7 @@ const SignUpForm = () => {
 
 
   return (
+
    <div
      style={{
         // background: `url(${backgroundImage})`,
@@ -75,7 +42,8 @@ const SignUpForm = () => {
         marginTop:'30px',
       }}
     >
-      <Container>
+    <Container>
+      
         <Row className="justify-content-center">
  
            <Col xs={12} md={6} className="d-flex flex-column justify-content-center align-items-center">
@@ -97,95 +65,147 @@ const SignUpForm = () => {
 
 
 
-           {/* Display a message for successful login */}
-          {isLoginSuccessful && (
+          {/* Conditionally render the server response message */}
+          {isLoginSuccessful ? (
             <p className="text-success mt-2">Sign-Up successful.</p>
+          ) : (
+            <p className="text-danger mt-3">{serverResponse}</p>
+          )}
 
-          )}
-      
-           {/* Display the server response message */}
-          {serverResponse && (
-            <p className="text-danger mt-3" >{serverResponse}</p>
-          )}
+
+              <Formik
+                initialValues={{
+                  firstname: '',
+                  lastname: '',
+                  email: '',
+                  password: '',
+                }}
+                validationSchema={validationSchema}
+
+                onSubmit={(values, actions) => {
+                  setLoading(true); // Set loading to true when the form is submitted
+
+                  axios.post('https://yeilva-store-server.up.railway.app/register', values)
+                    .then((response) => {
+                      if (response.status === 200) {
+                        setIsLoginSuccessful(true);
+                      } else {
+                        setIsLoginSuccessful(false);
+                        setServerResponse(response.data.error); // Set the error message
+                      }
+                    })
+                    .catch((error) => {
+                      console.error('Error submitting form data:', error);
+                      setIsLoginSuccessful(false);
+                      setServerResponse('Error: Email already registered');
+                    })
+                    .finally(() => {
+                      setLoading(false); // Set loading to false when the request is complete (success or failure)
+                      actions.setSubmitting(false); // Ensure form submission state is reset
+                    });
+                }}
+              >
+                {({
+                  values,
+                  errors,
+                  touched,
+                  handleChange,
+                  handleBlur,
+                  handleSubmit,
+                  isSubmitting,
+                }) => (
+
 
           <Form onSubmit={handleSubmit}>
-          <Form.Group controlId="firstname">
-          <Form.Label>First Name</Form.Label>
-          <Form.Control
-            type="text"
-            name="firstname" // Change to "firstname"
-            value={formData.firstname}
-            onChange={handleInputChange}
-            required
-          />
-        </Form.Group>
+                    <Form.Group controlId="firstname">
+                      <Form.Label>First Name</Form.Label>
+                      <Form.Control
+                        type="text"
+                        name="firstname"
+                        value={values.firstname}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        isInvalid={touched.firstname && !!errors.firstname}
+                        required
+                      />
+                      <Form.Control.Feedback type="invalid">{errors.firstname}</Form.Control.Feedback>
+                    </Form.Group>
 
         <Form.Group controlId="lastname">
           <Form.Label>Last Name</Form.Label>
           <Form.Control
             type="text"
             name="lastname" // Change to "lastname"
-            value={formData.lastname}
-            onChange={handleInputChange}
-            required
-          />
-        </Form.Group>
+             value={values.lastname}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        isInvalid={touched.lastname && !!errors.lastname}
+                        required
+                      />
+                      <Form.Control.Feedback type="invalid">{errors.lastname}</Form.Control.Feedback>
+                    </Form.Group>
 
         <Form.Group controlId="email">
           <Form.Label>Email Address</Form.Label>
           <Form.Control
             type="email"
             name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-            required
-          />
-        </Form.Group>
+              value={values.email}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        isInvalid={touched.email && !!errors.email}
+                        required
+                      />
+                      <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>
+                    </Form.Group>
 
         <Form.Group controlId="password">
           <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
             name="password"
-            value={formData.password}
-            onChange={handleInputChange}
-            required
-          />
-        </Form.Group>
+            value={values.password}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        isInvalid={touched.password && !!errors.password}
+                        required
+                      />
+                      <Form.Control.Feedback type="invalid">{errors.password}</Form.Control.Feedback>
+                    </Form.Group>
 
       
-        <Button variant="primary" type="submit" className="w-100 mt-3" disabled={loading}>
-        {loading ? <Spinner animation="border" size="sm" /> : 'Sign Up'}
-      </Button>
+        <Button variant="primary" type="submit" className="w-100 mt-3" disabled={loading || isSubmitting}>
+                      {loading ? <Spinner animation="border" size="sm" /> : 'Sign Up'}
+                    </Button>
+                  </Form>
+                )}
+              </Formik>
 
+            </div>
 
-        </Form>
+          <div style={{ maxWidth: '400px' }}>
+
+                <p  className=" mt-3"> Have an account?
+                <Link to="/login"  style={{marginTop:"20px", marginLeft:"10px",  textDecoration: 'none' }}>
+                  Login
+                </Link>
+                </p>
+
+             <p  className=" mt-3" > By signing up you Agree to YeilvaSTORE  <Link to="/termsandconditions" style={{ textDecoration: 'none' }} >
+              Terms of Service & Privacy Policy
+            </Link></p>    
+
         </div>
-      
-
-          <div style={{ maxWidth: '400px', }}>
-
-            <p  className=" mt-3"> Have an account?
-            <Link to="/login"  style={{marginTop:"20px", marginLeft:"10px",  textDecoration: 'none' }}>
-              Login
-            </Link>
-            </p>
-
-         <p  className=" mt-3" > By signing up you Agree to YeilvaSTORE  <Link to="/termsandconditions" style={{ textDecoration: 'none' }} >
-          Terms of Service & Privacy Policy
-        </Link></p>    
-
-        </div>
-      
-      </Col>
-      </Row>
-
+            
+          </Col>
+        </Row>
     </Container>
 
        {/* Footer Section */}
       <section className="w-100  mt-2" >
       <Footer />
       </section>
+
       </div>
   );
 };
