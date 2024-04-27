10014f59d0c7079ec5d7107452c555aa2adb541f
Fernando Arong <112321001+nyddanao11@users.noreply.github.com>
2024-04-26 20:02:44 -0700

    Update DealsData.js

--- src/data/DealsData.js
+++ src/data/DealsData.js
@@ -91,46 +91,4 @@ export const deals = [
     ],
 },
 
-{
-  id: 6,
-  name: "Jag Jeans",
-  url:  `${process.env.PUBLIC_URL}/images/jag.jpg`,
-  price:324.5,
-  discountedPrice: 649,
- percentage:"-50%",
-   weight:120,
-   rating: 4.2,
-  description:"Jag Jeans shirt for women, available in maroon color and medium size only",
-    thumbnails:
-      [`${process.env.PUBLIC_URL}/images/jag.jpg`,
-       `${process.env.PUBLIC_URL}/images/jag.jpg`,
-       `${process.env.PUBLIC_URL}/images/jag.jpg`,
-       
-      ],
-  productdetails:"for women, available in maroon color and medium size only, only one left",
-   reviews:[" no reviews "
-    ],
-},
-{
-  id: 7,
-  name: "J&G",
-  url:  `${process.env.PUBLIC_URL}/images/j&g.jpg`,
-  price: 299.5,
-  discountedPrice: 599,
-  percentage:"-50%",
-   weight:120,
-   rating: 4.2,
-  description:"J&G shirt for women, available in orange color and medium size only, only one left",
-    thumbnails:
-      [`${process.env.PUBLIC_URL}/images/j&g.jpg`,
-       `${process.env.PUBLIC_URL}/images/j&g.jpg`,
-       `${process.env.PUBLIC_URL}/images/j&g.jpg`,
-       
-      ],
-  productdetails:"J&G shirt for women, available in orange color and medium size only, only one left",
-   reviews:["no reviews"
-    ],
-},
-
-
 ];
