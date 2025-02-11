74e6371c1c68e437f305e8d5f39e59ee24038f28
Fernando Arong <112321001+nyddanao11@users.noreply.github.com>
2025-02-12 05:08:07 +0800

    Update FrozenFoodsData.js

--- src/components/Groceries/FrozenFoodsData.js
+++ src/components/Groceries/FrozenFoodsData.js
@@ -1,26 +1,26 @@
  export const Frozen = [
-    { id: 1, name: 'Hotdog1kl', category:'Frozen Food', price: 255, weight:500, url:`${process.env.PUBLIC_URL}/groceries/frozenfoods/hotdog01.png`, stock:2, 
+    { id: 1, name: 'Hotdog1kl', category:'Frozen Food', price: 255, weight:200, url:`${process.env.PUBLIC_URL}/groceries/frozenfoods/hotdog01.png`, stock:2, 
       thumbnails:
       [`${process.env.PUBLIC_URL}/groceries/frozenfoods/hotdog01.png`,
          `${process.env.PUBLIC_URL}/groceries/frozenfoods/hotdog01.png`,
       ],
      description:'Virginia premium Hotdog 1kg',
    },
-    { id: 2, name: 'Bacon01', category:'Frozen Food', price: 120, weight:500, url:`${process.env.PUBLIC_URL}/groceries/frozenfoods/bacon01.png`, stock:2, 
+    { id: 2, name: 'Bacon01', category:'Frozen Food', price: 120, weight:100, url:`${process.env.PUBLIC_URL}/groceries/frozenfoods/bacon01.png`, stock:2, 
        thumbnails:
       [`${process.env.PUBLIC_URL}/groceries/frozenfoods/bacon01.png`,
       `${process.env.PUBLIC_URL}/groceries/frozenfoods/bacon01.png`,
       ],
       description:'Virginia honey cured bacon 200g',
    },
-    { id: 3, name: 'Sweetham', category:'Frozen Food', price: 75, weight:500, url:`${process.env.PUBLIC_URL}/groceries/frozenfoods/sweetham.png`, stock:2, 
+    { id: 3, name: 'Sweetham', category:'Frozen Food', price: 75, weight:50, url:`${process.env.PUBLIC_URL}/groceries/frozenfoods/sweetham.png`, stock:2, 
         thumbnails:
       [`${process.env.PUBLIC_URL}/groceries/frozenfoods/sweetham.png`,
       `${process.env.PUBLIC_URL}/groceries/frozenfoods/sweetham.png`,
       ],
       description:'Winner sweet ham 250g',
    },
-    { id: 4, name: 'Cookedham', category:'Frozen Food', price: 90, weight:500, url:`${process.env.PUBLIC_URL}/groceries/frozenfoods/cookedham.png`, stock:2,
+    { id: 4, name: 'Cookedham', category:'Frozen Food', price: 90, weight:50, url:`${process.env.PUBLIC_URL}/groceries/frozenfoods/cookedham.png`, stock:2,
        thumbnails:
       [`${process.env.PUBLIC_URL}/groceries/frozenfoods/cookedham.png`,
       `${process.env.PUBLIC_URL}/groceries/frozenfoods/cookedham.png`,
