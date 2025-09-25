7bf49d7b31e97d4b4ee663763ad46bd7f2d394fa
Fernando Arong <112321001+nyddanao11@users.noreply.github.com>
2025-09-25 19:13:58 +0800

    Update ConfirmPage.js

--- src/pages/ConfirmPage.js
+++ src/pages/ConfirmPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import {Link} from'react-router-dom';
+import { Container, Row, Col, Alert, Spinner, Button } from 'react-bootstrap';
 import axios from 'axios';
 
 export default function ConfirmPage () {
